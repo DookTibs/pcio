fd9bbbc350b9bc504fb02a4284766700defe6d9c
Tom Feiler <tfeiler@gmail.com>
2020-06-08 13:21:40 -0500

    fix coloring issues

--- bottle_imp/src/bottle_imp.py
+++ bottle_imp/src/bottle_imp.py
@@ -9,6 +9,7 @@ red = "#FF0000"
 yellow = "#FFFF00"
 blue = "#0000FF"
 black = "#000000"
+white = "#FFFFFF"
 
 center_size = 80
 corner_size = 28
@@ -31,7 +32,7 @@ def round_rectangle(size, radius, fill):
     rectangle.paste(corner.rotate(270), (width - radius, 0))
     return rectangle
 
-def draw_card(label, background_color, points, filename):
+def draw_card(label, background_color, text_color, points, filename):
     # on pcio, set dimensions of card to 103x159, set border=no and rounded=yes options
     stroke_width = 3
 
@@ -43,12 +44,14 @@ def draw_card(label, background_color, points, filename):
     mid_y = height / 2
 
     # big number in middle
-    svg += f'\t<text x="{mid_x}" y="{mid_y}" fill="#000000" stroke="#FFFFFF" font-size="{center_size}" text-anchor="middle" alignment-baseline="middle" stroke-width=".7">{label}</text>\n'
+    fill = text_color
+    stroke = white if text_color == black else black
+    svg += f'\t<text x="{mid_x}" y="{mid_y}" fill="{fill}" stroke="{stroke}" font-size="{center_size}" text-anchor="middle" alignment-baseline="middle" stroke-width=".7">{label}</text>\n'
 
     # small number in corner
     corner_pad_x = 12
     corner_pad_y = 14
-    svg += f'\t<text x="{stroke_width + corner_pad_x}" y="{stroke_width + corner_pad_y}" fill="#000000" font-size="{corner_size}" text-anchor="middle" alignment-baseline="middle">{label}</text>\n'
+    svg += f'\t<text x="{stroke_width + corner_pad_x}" y="{stroke_width + corner_pad_y}" fill="{text_color}" font-size="{corner_size}" text-anchor="middle" alignment-baseline="middle">{label}</text>\n'
 
     # points
     if points > 0:
@@ -61,7 +64,7 @@ def draw_card(label, background_color, points, filename):
 
         i = 1
         while i <= points:
-            svg += f'\t<circle fill="black" cx="{dot_x}" cy="{dot_y}" r="{radius}"/>'
+            svg += f'\t<circle stroke="white" fill="black" cx="{dot_x}" cy="{dot_y}" r="{radius}"/>'
             dot_y += radius * 2.5
             i += 1
 
@@ -80,52 +83,53 @@ if __name__ == "__main__":
     i = 0
 
     cards = [
-        { "rank": 1, "points": 1, "color": yellow },
-        { "rank": 2, "points": 1, "color": yellow },
-        { "rank": 3, "points": 2, "color": yellow },
-        { "rank": 4, "points": 1, "color": blue },
-        { "rank": 5, "points": 2, "color": yellow },
-        { "rank": 6, "points": 1, "color": blue },
-        { "rank": 7, "points": 3, "color": yellow },
-        { "rank": 8, "points": 2, "color": blue },
-        { "rank": 9, "points": 3, "color": yellow },
-        { "rank": 10, "points": 2, "color": blue },
-        { "rank": 11, "points": 1, "color": red },
-        { "rank": 12, "points": 4, "color": yellow },
-        { "rank": 13, "points": 3, "color": blue },
-        { "rank": 14, "points": 1, "color": red },
-        { "rank": 15, "points": 4, "color": yellow },
-        { "rank": 16, "points": 2, "color": red },
-        { "rank": 17, "points": 3, "color": blue },
-        { "rank": 18, "points": 5, "color": yellow },
-        { "rank": 19, "points": 0, "color": black },
-        { "rank": 20, "points": 4, "color": blue },
-        { "rank": 21, "points": 2, "color": red },
-        { "rank": 22, "points": 5, "color": yellow },
-        { "rank": 23, "points": 3, "color": red },
-        { "rank": 24, "points": 4, "color": blue },
-        { "rank": 25, "points": 6, "color": yellow },
-        { "rank": 26, "points": 3, "color": red },
-        { "rank": 27, "points": 5, "color": blue },
-        { "rank": 28, "points": 6, "color": yellow },
-        { "rank": 29, "points": 4, "color": red },
-        { "rank": 30, "points": 5, "color": blue },
-        { "rank": 31, "points": 4, "color": red },
-        { "rank": 32, "points": 6, "color": blue },
-        { "rank": 33, "points": 5, "color": red },
-        { "rank": 34, "points": 6, "color": blue },
-        { "rank": 35, "points": 5, "color": red },
-        { "rank": 36, "points": 6, "color": red },
-        { "rank": 37, "points": 6, "color": red }
+        { "rank": 1, "points": 1, "bg_color": yellow, "text_color": black },
+        { "rank": 2, "points": 1, "bg_color": yellow, "text_color": black },
+        { "rank": 3, "points": 2, "bg_color": yellow, "text_color": black },
+        { "rank": 4, "points": 1, "bg_color": blue, "text_color": white },
+        { "rank": 5, "points": 2, "bg_color": yellow, "text_color": black },
+        { "rank": 6, "points": 1, "bg_color": blue, "text_color": white },
+        { "rank": 7, "points": 3, "bg_color": yellow, "text_color": black },
+        { "rank": 8, "points": 2, "bg_color": blue, "text_color": white },
+        { "rank": 9, "points": 3, "bg_color": yellow, "text_color": black },
+        { "rank": 10, "points": 2, "bg_color": blue, "text_color": white },
+        { "rank": 11, "points": 1, "bg_color": red, "text_color": black },
+        { "rank": 12, "points": 4, "bg_color": yellow, "text_color": black },
+        { "rank": 13, "points": 3, "bg_color": blue, "text_color": white },
+        { "rank": 14, "points": 1, "bg_color": red, "text_color": black },
+        { "rank": 15, "points": 4, "bg_color": yellow, "text_color": black },
+        { "rank": 16, "points": 2, "bg_color": red, "text_color": black },
+        { "rank": 17, "points": 3, "bg_color": blue, "text_color": white },
+        { "rank": 18, "points": 5, "bg_color": yellow, "text_color": black },
+        { "rank": 19, "points": 0, "bg_color": black, "text_color": black },
+        { "rank": 20, "points": 4, "bg_color": blue, "text_color": white },
+        { "rank": 21, "points": 2, "bg_color": red, "text_color": black },
+        { "rank": 22, "points": 5, "bg_color": yellow, "text_color": black },
+        { "rank": 23, "points": 3, "bg_color": red, "text_color": black },
+        { "rank": 24, "points": 4, "bg_color": blue, "text_color": white },
+        { "rank": 25, "points": 6, "bg_color": yellow, "text_color": black },
+        { "rank": 26, "points": 3, "bg_color": red, "text_color": black },
+        { "rank": 27, "points": 5, "bg_color": blue, "text_color": white },
+        { "rank": 28, "points": 6, "bg_color": yellow, "text_color": black },
+        { "rank": 29, "points": 4, "bg_color": red, "text_color": black },
+        { "rank": 30, "points": 5, "bg_color": blue, "text_color": white },
+        { "rank": 31, "points": 4, "bg_color": red, "text_color": black },
+        { "rank": 32, "points": 6, "bg_color": blue, "text_color": white },
+        { "rank": 33, "points": 5, "bg_color": red, "text_color": black },
+        { "rank": 34, "points": 6, "bg_color": blue, "text_color": white },
+        { "rank": 35, "points": 5, "bg_color": red, "text_color": black },
+        { "rank": 36, "points": 6, "bg_color": red, "text_color": black },
+        { "rank": 37, "points": 6, "bg_color": red, "text_color": black }
     ]
 
     for card in cards:
         card_rank = card.get("rank")
-        card_color = card.get("color")
+        card_color = card.get("bg_color")
+        text_color = card.get("text_color")
         card_points = card.get("points")
         zeroed_rank = "0" + str(card_rank) if card_rank < 10 else str(card_rank)
         filename = f"../cards/bottleimp_{zeroed_rank}.svg"
-        draw_card(card_rank, card_color, card_points, filename)
+        draw_card(card_rank, card_color, text_color, card_points, filename)
         csv_rows.append((f"bottleimp_{zeroed_rank}",f"https://raw.githubusercontent.com/DookTibs/pcio/master/bottle_imp/cards/bottleimp_{zeroed_rank}.svg"))
 
 
